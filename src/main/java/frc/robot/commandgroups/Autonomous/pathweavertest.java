2bfffba3c5a49d1cca5de140aa458d340cddd06a
Robotics <FirstRobotics@ashleyfurniture.com>
2022-02-17 16:44:31 -0600

    change

--- src/main/java/frc/robot/commandgroups/Autonomous/pathweavertest.java
+++ src/main/java/frc/robot/commandgroups/Autonomous/pathweavertest.java
@@ -2,14 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commandgroups;
+package frc.robot.commandgroups.Autonomous;
 
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
-import frc.robot.commands.drivebrake;
-import frc.robot.commands.shootercommand;
+import frc.robot.commands.Driving.drivebrake;
 import frc.robot.subsystems.drive;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
